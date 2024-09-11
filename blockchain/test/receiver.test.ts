103b4a918e9b538f228935ba84df9b18762c1e79
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2024-09-11 22:27:03 +0800

    refactor: rename wsReceiver to Dapp

--- blockchain/test/receiver.test.ts
+++ blockchain/test/receiver.test.ts
@@ -2,9 +2,8 @@ import { expect } from 'chai';
 import { ethers } from 'hardhat';
 import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers';
 
-import { DeviceNFT, DeviceReward, IoIDStoreMock, RiscZeroGroth16Verifier, WSReceiver } from '../typechain-types';
+import { DeviceNFT, DeviceReward, IoIDStoreMock, RiscZeroGroth16Verifier, Dapp, IoIDMock } from '../typechain-types';
 import { IMAGE_ID, IMAGE_ID_2, RAW_DATA } from './testData';
-import { IoIDMock } from '../typechain-types/contracts/mock/IoIDMock.sol';
 
 const PROJECT_1_ID = '0x036c';
 const PROVER_1_ID = '0x01';
@@ -24,60 +23,60 @@ const IOID_REGISTRY = '0x0A7e595C7889dF3652A19aF52C18377bF17e027D';
 const IOID_STORE = '0x60cac5CE11cb2F98bF179BE5fd3D801C3D5DBfF2';
 const DEVICE_ADD = '0x07b7265219329a3b30ab60b8d532b9e8990cc6e0';
 
-describe('Receiver', function () {
-  let receiver: WSReceiver;
+describe('Dapp', function () {
+  let dapp: Dapp;
   let rewards: DeviceReward;
   let ioidStore: IoIDStoreMock;
   let ioid: IoIDMock;
   let verifier: RiscZeroGroth16Verifier;
 
   beforeEach(async function () {
-    receiver = await loadFixture(deployWSReceiver);
+    dapp = await loadFixture(deployDapp);
     verifier = await loadFixture(deployVerifier);
 
-    const { store, token } = await setupIoID(receiver);
+    const { store, token } = await setupIoID(dapp);
     ioid = token;
     ioidStore = store;
 
-    // ALLOW RECEIVER TO MINT REWARDS
-    const rewardsAddr = await receiver.deviceRewards();
+    // ALLOW THE DAPP TO MINT REWARDS
+    const rewardsAddr = await dapp.deviceRewards();
     rewards = await ethers.getContractAt('DeviceReward', rewardsAddr);
     const MINTER_ROLE = await rewards.MINTER_ROLE();
-    await rewards.grantRole(MINTER_ROLE, receiver);
+    await rewards.grantRole(MINTER_ROLE, dapp);
   });
 
   describe('Initialization, setters and getters', () => {
     it('should include verifier address', async () => {
-      expect(await receiver.risc0Verifier()).to.eq(RISC0_VERIFIER);
+      expect(await dapp.risc0Verifier()).to.eq(RISC0_VERIFIER);
     });
     it('should include rewards address', async () => {
-      expect(await receiver.deviceRewards()).not.eq(ethers.ZeroAddress);
+      expect(await dapp.deviceRewards()).not.eq(ethers.ZeroAddress);
     });
     it('should include ioIDAddress', async () => {
-      expect(await receiver.ioID()).to.eq(ioid.target);
+      expect(await dapp.ioID()).to.eq(ioid.target);
     });
     it('should include ioidStore address', async () => {
-      expect(await receiver.ioIDStore()).to.eq(ioidStore.target);
+      expect(await dapp.ioIDStore()).to.eq(ioidStore.target);
     });
     it('should set and get verifier', async () => {
-      await receiver.setReceiver(RISC0_VERIFIER_2);
+      await dapp.setReceiver(RISC0_VERIFIER_2);
 
-      expect(await receiver.risc0Verifier()).to.eq(RISC0_VERIFIER_2);
+      expect(await dapp.risc0Verifier()).to.eq(RISC0_VERIFIER_2);
     });
     it('should set and get projectImageId', async () => {
-      await receiver.setProjectIdToImageId(PROJECT_1_ID, IMAGE_ID);
+      await dapp.setProjectIdToImageId(PROJECT_1_ID, IMAGE_ID);
 
-      expect(await receiver.getImageIdByProjectId(PROJECT_1_ID)).to.eq(IMAGE_ID.toLowerCase());
+      expect(await dapp.getImageIdByProjectId(PROJECT_1_ID)).to.eq(IMAGE_ID.toLowerCase());
     });
   });
   describe('Verification', () => {
     it('should emit verified event', async () => {
       const [sender] = await ethers.getSigners();
 
-      await receiver.setProjectIdToImageId(PROJECT_1_ID, IMAGE_ID);
-      await receiver.setReceiver(verifier.target);
-      await expect(receiver.process(PROJECT_1_ID, PROVER_1_ID, CLIENT_1_ID, RAW_DATA))
-        .to.emit(receiver, 'ProofVerified')
+      await dapp.setProjectIdToImageId(PROJECT_1_ID, IMAGE_ID);
+      await dapp.setReceiver(verifier.target);
+      await expect(dapp.process(PROJECT_1_ID, PROVER_1_ID, CLIENT_1_ID, RAW_DATA))
+        .to.emit(dapp, 'ProofVerified')
         .withArgs(
           sender.address,
           IMAGE_ID.toLowerCase(),
@@ -85,64 +84,64 @@ describe('Receiver', function () {
         );
     });
     it('should revert if imageId is invalid', async () => {
-      await receiver.setProjectIdToImageId(PROJECT_1_ID, IMAGE_ID_2);
-      await receiver.setReceiver(verifier.target);
-      await expect(receiver.process(PROJECT_1_ID, PROVER_1_ID, CLIENT_1_ID, RAW_DATA)).to.be.revertedWithCustomError(
-        receiver,
+      await dapp.setProjectIdToImageId(PROJECT_1_ID, IMAGE_ID_2);
+      await dapp.setReceiver(verifier.target);
+      await expect(dapp.process(PROJECT_1_ID, PROVER_1_ID, CLIENT_1_ID, RAW_DATA)).to.be.revertedWithCustomError(
+        dapp,
         'VerificationFailed',
       );
     });
     it('should not process if verifier not set yet', async () => {
-      await receiver.setReceiver(ethers.ZeroAddress);
+      await dapp.setReceiver(ethers.ZeroAddress);
 
-      await expect(receiver.process(PROJECT_1_ID, PROVER_1_ID, CLIENT_1_ID, RAW_DATA)).to.be.revertedWithCustomError(
-        receiver,
+      await expect(dapp.process(PROJECT_1_ID, PROVER_1_ID, CLIENT_1_ID, RAW_DATA)).to.be.revertedWithCustomError(
+        dapp,
         'VerifierNotSet',
       );
     });
     it('should revert if project image id not found', async () => {
-      await expect(receiver.process(PROJECT_1_ID, PROVER_1_ID, CLIENT_1_ID, RAW_DATA)).to.be.revertedWithCustomError(
-        receiver,
+      await expect(dapp.process(PROJECT_1_ID, PROVER_1_ID, CLIENT_1_ID, RAW_DATA)).to.be.revertedWithCustomError(
+        dapp,
         'ImageIdNotFound',
       );
     });
   });
   describe('Rewards distribution', () => {
     beforeEach(async () => {
-      await receiver.setProjectIdToImageId(PROJECT_1_ID, IMAGE_ID);
-      await receiver.setReceiver(verifier.target);
+      await dapp.setProjectIdToImageId(PROJECT_1_ID, IMAGE_ID);
+      await dapp.setReceiver(verifier.target);
     });
     it('should mint tokens to the device owner', async () => {
       const [, deviceOwner] = await ethers.getSigners();
 
-      await receiver.process(PROJECT_1_ID, PROVER_1_ID, CLIENT_1_ID, RAW_DATA);
+      await dapp.process(PROJECT_1_ID, PROVER_1_ID, CLIENT_1_ID, RAW_DATA);
       expect(await rewards.balanceOf(deviceOwner)).to.eq(REWARDS_AMOUNT);
     });
   });
 });
 
 describe('Distribution reverts', () => {
-  let receiver: WSReceiver;
+  let dapp: Dapp;
   let rewards: DeviceReward;
   let verifier: RiscZeroGroth16Verifier;
 
   beforeEach(async function () {
-    receiver = await loadFixture(deployWSReceiver);
+    dapp = await loadFixture(deployDapp);
     verifier = await loadFixture(deployVerifier);
 
-    // ALLOW RECEIVER TO MINT REWARDS
-    const rewardsAddr = await receiver.deviceRewards();
+    // ALLOW THE DAPP TO MINT REWARDS
+    const rewardsAddr = await dapp.deviceRewards();
     rewards = await ethers.getContractAt('DeviceReward', rewardsAddr);
     const MINTER_ROLE = await rewards.MINTER_ROLE();
-    await rewards.grantRole(MINTER_ROLE, receiver);
+    await rewards.grantRole(MINTER_ROLE, dapp);
 
-    await receiver.setProjectIdToImageId(PROJECT_1_ID, IMAGE_ID);
-    await receiver.setReceiver(verifier.target);
+    await dapp.setProjectIdToImageId(PROJECT_1_ID, IMAGE_ID);
+    await dapp.setReceiver(verifier.target);
   });
 
   it('should revert if project device contract not set', async () => {
-    await expect(receiver.process(PROJECT_1_ID, PROVER_1_ID, CLIENT_1_ID, RAW_DATA))
-      .to.be.revertedWithCustomError(receiver, 'DeviceContractNotSet')
+    await expect(dapp.process(PROJECT_1_ID, PROVER_1_ID, CLIENT_1_ID, RAW_DATA))
+      .to.be.revertedWithCustomError(dapp, 'DeviceContractNotSet')
       .withArgs(PROJECT_1_ID);
   });
 });
@@ -152,13 +151,13 @@ const deployERC20 = async () => {
   return factory.deploy();
 };
 
-const deployWSReceiver = async () => {
+const deployDapp = async () => {
   const deviceRewards = await deployERC20();
   const ioidStore = await deployIoIDStore();
   const ioid = await deployIoID();
 
-  const WSReceiver = await ethers.getContractFactory('WSReceiver');
-  return WSReceiver.deploy(RISC0_VERIFIER, deviceRewards, ioid, ioidStore);
+  const Dapp = await ethers.getContractFactory('Dapp');
+  return Dapp.deploy(RISC0_VERIFIER, deviceRewards, ioid, ioidStore);
 };
 
 const deployVerifier = async () => {
@@ -191,20 +190,20 @@ const deployIoID = async () => {
   return factory.deploy();
 };
 
-const setupIoID = async (receiver: WSReceiver) => {
+const setupIoID = async (dapp: Dapp) => {
   const deviceNft = await loadFixture(deployDeviceNFT);
 
-  const ioidStore = await setDeviceContract(receiver, deviceNft);
+  const ioidStore = await setDeviceContract(dapp, deviceNft);
   const wallet = await deployDeviceWallet();
   // MINT DEVICE NFT 0 TO THE DEVICE WALLET
   await deviceNft.mint(wallet, 0);
-  const ioid = await mintIoIDToOwner(receiver);
+  const ioid = await mintIoIDToOwner(dapp);
 
   return { store: ioidStore, token: ioid };
 };
 
-const setDeviceContract = async (receiver: WSReceiver, deviceNft: DeviceNFT) => {
-  const ioidStoreAddr = await receiver.ioIDStore();
+const setDeviceContract = async (dapp: Dapp, deviceNft: DeviceNFT) => {
+  const ioidStoreAddr = await dapp.ioIDStore();
   const ioidStore = await ethers.getContractAt('IoIDStoreMock', ioidStoreAddr);
   await ioidStore.setDeviceContract(PROJECT_1_ID, deviceNft);
 
@@ -221,10 +220,10 @@ const deployDeviceWallet = async () => {
   return wallet;
 };
 
-const mintIoIDToOwner = async (receiver: WSReceiver) => {
+const mintIoIDToOwner = async (dapp: Dapp) => {
   const [, deviceOwner] = await ethers.getSigners();
 
-  const ioidAddr = await receiver.ioID();
+  const ioidAddr = await dapp.ioID();
   const ioid = await ethers.getContractAt('IoIDMock', ioidAddr);
   await ioid.mint(PROJECT_1_ID, DEVICE_ADD, deviceOwner);
 
